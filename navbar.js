98adf0fbfcb6b27fb2cf606aae684cf6436f81ff
Franco <francopanozzo@hotmail.com.ar>
2020-12-10 13:36:38 +0100

    Added functionality to the navbar on scroll and some transitions

---
+++ navbar.js
@@ -0,0 +1,47 @@
+// Functionality to open and close the navigation menu
+// const navToggleBtn = document.querySelector('.nav-toggle');
+const navbar = document.querySelector('.navbar');
+
+// navToggleBtn.addEventListener('click', () => {
+//   const navIsFixed = Array.from(header.classList).includes('fixed');
+//   header.classList.toggle('active');
+//   if (!navIsFixed) {
+//     header.classList.add('fixed');
+//   }
+//   handleScroll();
+// });
+
+// Fucntionality to fix the navbar when scrolled
+// const debounce = (fn) => {
+//   let frame;
+//   return (...params) => {
+//     if (frame) {
+//       cancelAnimationFrame(frame);
+//     }
+//     frame = requestAnimationFrame(() => {
+//       fn(...params);
+//     });
+//   };
+// };
+
+const handleScroll = () => {
+  // don't action if the navbar is on active state
+  if (Array.from(navbar.classList).includes('active')) return;
+  if (navbar.className)
+    if (window.scrollY > 50) {
+      navbar.classList.add('fixed');
+    } else {
+      navbar.classList.remove('fixed');
+    }
+};
+
+document.addEventListener('scroll', handleScroll, { passive: true });
+
+// Functionality to close the navbar when a nav link is clicked
+// const closeNavbar = (e) => {
+//   // Checks if an acnhor tag is clicked
+//   if (e.target.tagName.toLowerCase() !== 'a') return;
+//   header.classList.remove('active');
+// };
+
+// document.querySelector('.nav-menu').addEventListener('click', closeNavbar);
