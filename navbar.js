393bc61b7b992f2bd877574db4fe16e6714835a6
Franco <francopanozzo@hotmail.com.ar>
2020-12-21 15:05:19 +0100

    Navbar redesign, mobile first approach with functionality and looks ready for mobile, desktop unfinished

--- navbar.js
+++ navbar.js
@@ -1,32 +1,29 @@
 // Functionality to open and close the navigation menu
-// const navToggleBtn = document.querySelector('.nav-toggle');
-// const navbar = document.querySelector('.navbar');
-
-// navToggleBtn.addEventListener('click', () => {
-//   const navIsFixed = Array.from(header.classList).includes('fixed');
-//   header.classList.toggle('active');
-//   if (!navIsFixed) {
-//     header.classList.add('fixed');
-//   }
-//   handleScroll();
-// });
-
-// const debounce = (fn) => {
-//   let frame;
-//   return (...params) => {
-//     if (frame) {
-//       cancelAnimationFrame(frame);
-//     }
-//     frame = requestAnimationFrame(() => {
-//       fn(...params);
-//     });
-//   };
-// };
-
-// Fucntionality to fix the navbar when scrolled
-
+const navToggleBtn = document.querySelector('.menu-toggle');
 const navbar = document.querySelector('.navbar');
 
+navToggleBtn.addEventListener('click', () => {
+  const navIsFixed = Array.from(navbar.classList).includes('fixed');
+  navbar.classList.toggle('active');
+  if (!navIsFixed) {
+    navbar.classList.add('fixed');
+  }
+  handleScroll();
+});
+
+const debounce = (fn) => {
+  let frame;
+  return (...params) => {
+    if (frame) {
+      cancelAnimationFrame(frame);
+    }
+    frame = requestAnimationFrame(() => {
+      fn(...params);
+    });
+  };
+};
+
+// Fucntionality to fix the navbar when scrolled
 const handleScroll = () => {
   // don't action if the navbar is on active state
   if (Array.from(navbar.classList).includes('active')) return;
@@ -65,10 +62,10 @@ document.addEventListener('scroll', handleScroll, { passive: true });
 // sectionOneObserver.observe(sectionOne);
 
 // Functionality to close the navbar when a nav link is clicked
-// const closeNavbar = (e) => {
-//   // Checks if an acnhor tag is clicked
-//   if (e.target.tagName.toLowerCase() !== 'a') return;
-//   header.classList.remove('active');
-// };
+const closeNavbar = (e) => {
+  // Checks if an acnhor tag is clicked
+  if (e.target.tagName.toLowerCase() !== 'a') return;
+  navbar.classList.remove('active');
+};
 
-// document.querySelector('.nav-menu').addEventListener('click', closeNavbar);
+document.querySelector('.navbar ul').addEventListener('click', closeNavbar);
